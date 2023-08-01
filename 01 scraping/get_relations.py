4d12d3cd83c10f0bc15aac0261c4c540c8e19e9d
Victor-Gurbani <89997914+victor-gurbani@users.noreply.github.com>
2023-08-01 22:44:43 +0100

    File error handling

--- 01 scraping/get_relations.py
+++ 01 scraping/get_relations.py
@@ -1,9 +1,11 @@
 import instaloader
 import time
 import sys
-from alive_progress import alive_bar
+#from alive_progress import alive_bar
 import copy
 import json
+import os
+import platform
 
 print("Starting") 
 
@@ -17,17 +19,27 @@ relations_file = 'relations.txt'
 my_followers = []
 my_followers_left = []
 
-with open('followers.txt') as f:
-    my_followers = [line.strip() for line in f.readlines()]
+try:
+    with open('followers.txt') as f:
+        my_followers = [line.strip() for line in f.readlines()]
+except FileNotFoundError:
+    print("followers.txt not found. Please make sure the file exists.")
 
-with open('my_followers_left.txt') as f:
-    my_followers_left = [line.strip() for line in f.readlines()]
+try:
+    with open('my_followers_left.txt') as f:
+        my_followers_left = [line.strip() for line in f.readlines()]
+except FileNotFoundError:
+    print("my_followers_left.txt not found. Creating a new file.")
+    with open('my_followers_left.txt', 'w') as f:
+        for follower in my_followers:
+            f.write(f"{follower}\n")
+    my_followers_left = my_followers.copy()
 
 if not my_followers_left:
-  with open('my_followers_left.txt', 'w') as f:
-    for follower in my_followers:
-      f.write(f"{follower}\n")
-  my_followers_left = my_followers.copy()
+    with open('my_followers_left.txt', 'w') as f:
+        for follower in my_followers:
+            f.write(f"{follower}\n")
+    my_followers_left = my_followers.copy()
   
 try:
   with open(relations_file, 'a') as f:
@@ -57,7 +69,15 @@ try:
         print("Mutual: ", countMutual, " followees")
         if countMutual == 0:
             sys.exit()
-        print('sed -i "" -e "1d" my_followers_left.txt')
+
+        if platform.system() == 'Windows':
+            with open('my_followers_left.txt', 'r') as f:
+                lines = f.readlines()
+            with open('my_followers_left.txt', 'w') as f:
+                f.writelines(lines[1:])
+        else:
+            os.system('sed -i "" -e "1d" my_followers_left.txt')
+        
         print("Exit now if necessary\r", end="")
         time.sleep(10)
 
