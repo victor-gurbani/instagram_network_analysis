5002144ab5226a790a0a6e7d3d5edd04fc9a1de6
Victor-Gurbani <89997914+victor-gurbani@users.noreply.github.com>
2024-12-15 19:30:06 +0100

    Added gitignore (to allow easier updates) and last update to animations

--- 01 scraping/get_relations.py
+++ 01 scraping/get_relations.py
@@ -63,7 +63,7 @@ try:
             time.sleep(0.5)
             print("\rProcessing followees", end="")
 
-            animation = [' |', '/ ', ' -', ' \\']
+            animation = [' |', ' /', ' -', ' \\']
             anim_index = 0
             countMutual = 0
             for followee in tempFollowees:
@@ -76,8 +76,7 @@ try:
                     f.flush()
                     countMutual += 1
 
-            if not no_animation:
-                print(" \r", end="")  # Clear the whole line after animation (works even if animation is disabled)
+            print(" \r", end="")  # Clear the whole line after animation (works even if animation is disabled)
 
             print("Mutual: ", countMutual, " followees")
             
