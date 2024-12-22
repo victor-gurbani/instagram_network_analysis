60b407a913c369d1974f61ddbc17e97ad2fb7799
Victor-Gurbani <89997914+victor-gurbani@users.noreply.github.com>
2024-12-22 13:18:47 +0000

    also create a followees txt (so that the directed graph will then also include this for you

--- 01 scraping/get_my_followers.py
+++ 01 scraping/get_my_followers.py
@@ -16,14 +16,26 @@ L.load_session_from_file(username)
 # Obtain profile metadata
 profile = instaloader.Profile.from_username(L.context, username)
 
-# Print list of followees
-follow_list = []
+# Print list of followers
+followers_list = []
 count = 0
-for followee in profile.get_followers():
+for follower in profile.get_followers():
   sleep(0.1)
-  follow_list.append(followee.username)
+  followers_list.append(follower.username)
   with open("followers.txt", "a+") as file:
-    file.write(follow_list[count])
+    file.write(followers_list[count])
     file.write("\n")
-  print(follow_list[count])
+  print(followers_list[count])
+  count += 1
+
+# Print list of followees
+followees_list = []
+count = 0
+for followee in profile.get_followees():
+  sleep(0.1)
+  followees_list.append(followee.username)
+  with open("followees.txt", "a+") as file:
+    file.write(followees_list[count])
+    file.write("\n")
+  print(followees_list[count])
   count += 1
